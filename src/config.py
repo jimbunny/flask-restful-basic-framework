02c94a5de50a2a5b94a6b3ce4693c5a83fcf45e3
jingtong.yu <jingtong.yu@china.zhaogang.com>
2018-02-06 18:58:51 +0800

    feat：add config file

---
+++ src/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by jim on 2018/2/6
+
+# 启动方式
+HOST = "0.0.0.0"
+# 启动端口
+PORT = 8888
+# 启动模式
+DEBUG = True
--- src/run.py
+++ src/run.py
@@ -3,7 +3,12 @@
 # Created by jim on 2018/1/30
 
 from flask import Flask
-app = Flask(__name__)
+APP_NAME = "flask-restful-api"
+app = Flask(APP_NAME, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+print(app.config["HOST"])
+# 现在通过app.config["VAR_NAME"]，我们可以访问到对应的变量
 
 
 @app.route('/')
@@ -16,4 +21,8 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8888, debug=True)
+    # 读取配置文件配置
+    host = app.config["HOST"]
+    port = app.config["PORT"]
+    debug = app.config["DEBUG"]
+    app.run(host=host, port=port, debug=debug)
